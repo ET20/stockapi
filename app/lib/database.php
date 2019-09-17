f8af39aa995e3187f84fcecf1fc0ba6fafd7090c
waltermolina <waltermolina@msn.com>
2019-09-17 12:56:18 -0300

    Arreglo en la Conexión a la DB

--- app/lib/database.php
+++ app/lib/database.php
@@ -7,11 +7,11 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', '1234');
-        
+        $pdo = new PDO('mysql:host=66.97.35.95;port=3306;dbname=antoniob_stock', 'titoet20', 'Tito2019');
+
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-        
+
         return $pdo;
     }
 }
