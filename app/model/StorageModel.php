56be31652ac148795c85b76067eb4030e0078b0d
rafael <alguiencoollk@gmail.com>
2019-09-18 10:40:21 -0300

    Hola

---
+++ app/model/StorageModel.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Model; //Donde estamos
+
+use App\Lib\Database; 
+use App\Lib\Response;
+class StorageModel {
+    //Rafael Perez! pero perez de pereza!
+    private $db;
+    private $stro = 'almacen';
+    private $ta = "tipoalmacen";
+    private $response;
+    public function __CONSTRUCT() {
+        $this->db = Database::StartUp();
+        $this->response = new Response();
+    }
+    public function GetAll() {
+        try {
+            $stm = $this->db->prepare(
+                "select s.*,t.idtipoalmacen,t.descripcion from 
+                $this->stro s
+            join $this->ta t on s.tipo = t.idtipoalmacen" );
+            $stm->execute();
+            $this->response->setResponse(true);
+            $this->response->result = $stm->fetchAll(); 
+            return $this->response;
+        } catch (Exception $e) {
+            $this->response->setResponse(false, $e->getMessage());
+            return $this->response;
+        }
+    }
+}
