02cac2fbee3f748a62468bd33323936d49343e88
RafaelPerez19 <51133750+RafaelPerez19@users.noreply.github.com>
2019-10-01 15:02:35 -0300

    Add files via upload

---
+++ app/route/StorageRoute.php
@@ -0,0 +1,60 @@
+<?php
+use App\Model\StorageModel;
+
+$app->group('/storage', function () {
+
+    $this->get('/almacen', function ($req, $res, $args) {
+        $modelo = new StorageModel();
+
+        $res
+            ->getBody()
+            ->write(
+                json_encode(
+                    $modelo->GetAll() // Qué función usaré de mi modelo
+                )
+            );
+
+        return $res->withHeader(
+            'Content-type',
+            'application/json; charset=utf-8'
+        );
+
+    });
+
+    $this->get('/{id}', function ($req, $res, $args) {
+        $um = new StorageModel();
+
+        $res
+            ->getBody()
+            ->write(
+                json_encode(
+                    $um->Get($args['id'])
+                )
+            );
+        return $res->withHeader(
+            'Content-type',
+            'application/json; charset=utf-8'
+        );
+
+    });
+
+    $this->post('', function ($req, $res) {
+        $um = new StorageModel();
+        
+        $res
+           ->getBody()
+           ->write(
+            json_encode(
+                $um->InsertOrUpdate(
+                    $req->getParsedBody()
+                )
+            )
+        );
+        
+        return $res->withHeader(
+            'Content-type',
+            'application/json; charset=utf-8'
+        );
+    });
+
+});
