25d4f60bf9e5d1f860380051be2b4d197e4985af
Martin087 <Martin_godeken@hotmail.com>
2019-10-02 09:52:44 -0300

    corregí algunos metodos

--- app/route/containersRoute.php
+++ app/route/containersRoute.php
@@ -36,7 +36,25 @@ $app->group('/envase', function () {
         );
 
     });
-    $this->post('', function ($req, $res) {
+    $this->post('/', function ($req, $res) {
+        $um = new containersModel();
+
+        $res
+            ->getBody()
+            ->write(
+                json_encode(
+                    $um->Insert(
+                        $req->getParsedBody()
+                    )
+                )
+            );
+
+        return $res->withHeader(
+            'Content-type',
+            'application/json; charset=utf-8'
+        );
+    });
+    /*$this->post('', function ($req, $res) {
         $um = new containersModel();
 
         return $res
@@ -52,8 +70,26 @@ $app->group('/envase', function () {
                     )
                 )
             );
+    });*/
+    $this->put('/', function ($req, $res) {
+        $um = new containersModel();
+
+        $res
+            ->getBody()
+            ->write(
+                json_encode(
+                    $um->Update(
+                        $req->getParsedBody()
+                    )
+                )
+            );
+
+        return $res->withHeader(
+            'Content-type',
+            'application/json; charset=utf-8'
+        );
     });
-    $this->put('', function ($req, $res) {
+    /*$this->put('', function ($req, $res) {
         $um = new containerModel();
 
         return $res
@@ -69,8 +105,24 @@ $app->group('/envase', function () {
                     )
                 )
             );
+    });*/
+    $this->delete('/{id}', function ($req, $res, $args) {
+        $um = new containersModel();
+
+        $res
+            ->getBody()
+            ->write(
+                json_encode(
+                    $um->Delete($args['id'])
+                )
+            );
+
+        return $res->withHeader(
+            'Content-type',
+            'application/json; charset=utf-8'
+        );
     });
-    $this->delete('', function ($req, $res) {
+    /*$this->delete('', function ($req, $res) {
         $um = new containersModel();
 
         return $res
@@ -87,5 +139,5 @@ $app->group('/envase', function () {
                 )
             );
 
-    });
+    });*/
 });
