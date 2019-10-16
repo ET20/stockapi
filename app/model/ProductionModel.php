e53acaf790bb1ff529f0ee4c5fedebb721d869b1
WalterDLucero <49043274+WalterDLucero@users.noreply.github.com>
2019-10-16 09:58:57 -0300

    jeje

--- app/model/ProductionModel.php
+++ app/model/ProductionModel.php
@@ -4,7 +4,7 @@ namespace App\Model; //Donde estamos
 use App\Lib\Database; //Importamos el archivo que conecta a la base de datos
 use App\Lib\Response;
 //Importamos el archivo que arma la respuesta
-
+//jejje
 class ProductionModel
 { //Nombre de la clase
     private $db;
